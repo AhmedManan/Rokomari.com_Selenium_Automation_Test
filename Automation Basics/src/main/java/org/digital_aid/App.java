8c2190f24c2fcaa06fbcad41326bbada8d2e85bd
Manan Ahmed Broti <54348038+AhmedManan@users.noreply.github.com>
2023-11-20 22:30:16 +0600

    initial commit

---
+++ Automation Basics/src/main/java/org/digital_aid/App.java
@@ -0,0 +1,13 @@
+package org.digital_aid;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
