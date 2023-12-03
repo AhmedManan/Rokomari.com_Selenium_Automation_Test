2368839af0264db1325f1b589e623aa21887e87f
Manan Ahmed Broti <54348038+AhmedManan@users.noreply.github.com>
2023-12-03 21:57:04 +0600

    BaseURL Test

--- AutomationBasic/src/test/java/AutomationBasic/BaseUrlTest.java
+++ AutomationBasic/src/test/java/AutomationBasic/BaseUrlTest.java
@@ -1,5 +1,6 @@
 package AutomationBasic;
 
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 public class BaseUrlTest extends BaseDriver{
@@ -7,7 +8,12 @@ public class BaseUrlTest extends BaseDriver{
 	String Url="https://digital-aid.ahmedmanan.com/";
 	
 	@Test
-	public void OpenUrl(){
+	public void OpenUrl() throws InterruptedException{
 		driver.get(Url);
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.cssSelector("html > body > div > div > div > div > div > div > h1")).click();
+		
+		Thread.sleep(5000); // 1000 = 1 sec 
 	}
 }
