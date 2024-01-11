7b75c37f7b670ff4d50b73c01b8ee70788f8dbba
Manan Ahmed Broti <54348038+AhmedManan@users.noreply.github.com>
2024-01-12 03:17:55 +0600

    BaseURL Test

--- AutomationBasic/src/test/java/AutomationBasic/BaseUrlTest.java
+++ AutomationBasic/src/test/java/AutomationBasic/BaseUrlTest.java
@@ -1,6 +1,7 @@
 package AutomationBasic;
 
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class BaseUrlTest extends BaseDriver{
@@ -12,8 +13,13 @@ public class BaseUrlTest extends BaseDriver{
 		driver.get(Url);
 		driver.manage().window().maximize();
 		
-		driver.findElement(By.cssSelector("html > body > div > div > div > div > div > div > h1")).click();
-		
-		Thread.sleep(5000); // 1000 = 1 sec 
+        // Find the element
+        String actualText = driver.findElement(By.cssSelector("html > body > div > div > div > div > div > div > h1")).getText();
+
+        // Compare the text with the expected value
+        String expectedText = "একটি মশার কামড়ও হতে পারে বিপদজনক!";
+        Assert.assertEquals(actualText, expectedText, "Text does not match the expected value.");
+
+        Thread.sleep(5000); // 1000 = 1 sec
 	}
 }
