4d5c846f70d0d9c34cf777a929906baf269641a4
Manan Ahmed Broti <54348038+AhmedManan@users.noreply.github.com>
2024-02-15 23:25:14 +0600

    Login Automation

---
+++ AutomationBasic/src/test/java/AutomationBasic/LoginAutomation.java
@@ -0,0 +1,27 @@
+package AutomationBasic;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class LoginAutomation extends BaseDriver {
+	
+	String Url="https://www.wafilife.com/my-account";
+	
+	@Test
+	public void SelectWritter() throws InterruptedException {
+		
+		driver.get(Url);	
+        driver.manage().window().maximize();
+		Thread.sleep(2000); // 1000 = 1sec
+		
+		driver.findElement(By.xpath("//input[@id='username']")).clear();
+		driver.findElement(By.xpath("//input[@id='username']")).sendKeys("manan.sqa@protonmail.com");
+		driver.findElement(By.xpath("//input[@id='password']")).clear();
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("@WafiLife");
+		driver.findElement(By.name("login")).click();
+		Thread.sleep(5000); // 1000 = 1sec
+		
+        
+	}
+
+}
